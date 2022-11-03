fcf6fa6f6791e1035f565b686efd3527162dabf3
jossi <rolety2202@naver.com>
2022-11-03 14:11:27 +0900

    테스트

--- src/main/java/com/emergency/challenge/chat/config/StompHandler.java
+++ src/main/java/com/emergency/challenge/chat/config/StompHandler.java
@@ -97,18 +97,18 @@ public class StompHandler implements ChannelInterceptor {
 //                    .get("simpUser")).map(Principal::getName).orElse("UnknownUser");
 
             //이름 그냥 넣어주기 로그인 정보에서
-            Member member = tokenProvider.getMemberFromAuthentication();
+//            Member member = tokenProvider.getMemberFromAuthentication();
             System.out.println("message12312412412421412412421 = " + message);
             System.out.println(" simpleUser= " +message.getHeaders()
                     .get("simpUser"));
-            System.out.println("name111111111111111111111111111111111111 = " + member.getNickName());
+//            System.out.println("name111111111111111111111111111111111111 = " + member.getNickName());
 
             redisSubscriber.sendMessage(ChatMessage.builder()
                     .type(ChatMessage.MessageType.ENTER)
                     .roomId(roomId)
                     .sender("JOSSI")
                     .build());
-            log.info("SUBSCRIBED {}, {}", member.getNickName(), roomId);
+            log.info("SUBSCRIBED {}, {}", "jossi", roomId);
         }
 
         else if (StompCommand.DISCONNECT == accessor.getCommand()){
